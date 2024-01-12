e00872ce745c127b9875f64ecc6ff44719b06eec
zecaribeiro <jcribeiro.bcc@gmail.com>
2024-01-12 02:25:39 -0500

    Adding the midnight theme

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -12,111 +12,132 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local config = function(plugin)
-    return function ()
+    return function()
         require(plugin).setup()
     end
 end
 
 require("lazy").setup({
     {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.2',
-        dependencies = { 'nvim-lua/plenary.nvim' },
-        config = config("plugin.plenary")
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.2",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = config("plugin.plenary"),
     },
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            vim.cmd.colorscheme "catppuccin-macchiato"
-        end
-    },
+    --{
+    --    "catppuccin/nvim",
+    --    name = "catppuccin",
+    --    priority = 1000,
+    --    config = function()
+    --        --require("catppuccin").setup({
+    --        --    color_overrides = {
+    --        --        all = {
+    --        --            base = "#000000",
+    --        --            mantle = "#000000",
+    --        --            crust = "#000000",
+    --        --        },
+    --        --    },
+    --        --})
+    --        vim.cmd.colorscheme("catppuccin-macchiato")
+    --    end,
+    --},
     --{
     --    'navarasu/onedark.nvim',
     --    priority = 1000,
     --    config = function()
-    --        vim.cmd.colorscheme 'onedark'
+    --        require('onedark').setup {
+    --            style = 'darker'
+    --        }
+    --        require('onedark').load()
     --    end
     --},
-    {
-        'nvim-treesitter/nvim-treesitter',
-        build = ':TSUpdate',
-        config = config("plugin.treesitter")
+    --{
+    --    'Mofiqul/vscode.nvim',
+    --    config = function ()
+    --        require('vscode').load('dark')
+    --    end
+    --},
+    { 'dasupradyumna/midnight.nvim', lazy = false, priority = 1000,
+        config = function ()
+            vim.cmd.colorscheme 'midnight'
+        end
     },
     {
-        'kyazdani42/nvim-tree.lua',
-        config = config("plugin.nvimtree")
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = config("plugin.treesitter"),
     },
     {
-        'akinsho/bufferline.nvim',
-        dependencies = { 'kyazdani42/nvim-tree.lua' },
-        config = config("plugin.bufferline")
+        "kyazdani42/nvim-tree.lua",
+        config = config("plugin.nvimtree"),
     },
     {
-        'nvim-tree/nvim-web-devicons',
-        config = config("plugin.devicons")
+        "akinsho/bufferline.nvim",
+        dependencies = { "kyazdani42/nvim-tree.lua" },
+        config = config("plugin.bufferline"),
     },
     {
-        'famiu/bufdelete.nvim'
+        "nvim-tree/nvim-web-devicons",
+        config = config("plugin.devicons"),
+    },
+    {
+        "famiu/bufdelete.nvim",
     },
     {
         "windwp/nvim-autopairs",
-        config = config("nvim-autopairs")
+        config = config("nvim-autopairs"),
     },
     {
         "williamboman/mason-lspconfig.nvim",
-        config = config("plugin.mason-lspconfig")
+        config = config("plugin.mason-lspconfig"),
     },
     {
         "williamboman/mason.nvim",
-        config = config("plugin.mason")
+        config = config("plugin.mason"),
     },
     {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v2.x",
         config = config("plugin.lsp"),
         dependencies = {
             -- LSP Support
-            {'neovim/nvim-lspconfig'},
-            {'williamboman/mason.nvim'},
-            {'williamboman/mason-lspconfig.nvim'},
+            { "neovim/nvim-lspconfig" },
+            { "williamboman/mason.nvim" },
+            { "williamboman/mason-lspconfig.nvim" },
             -- Autocompletion
-            {'hrsh7th/nvim-cmp'},
-            {'hrsh7th/cmp-buffer'},
-            {'hrsh7th/cmp-path'},
-            {'saadparwaiz1/cmp_luasnip'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'hrsh7th/cmp-nvim-lua'}
-        }
+            { "hrsh7th/nvim-cmp" },
+            { "hrsh7th/cmp-buffer" },
+            { "hrsh7th/cmp-path" },
+            { "saadparwaiz1/cmp_luasnip" },
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "hrsh7th/cmp-nvim-lua" },
+        },
     },
     {
-        'nvim-lualine/lualine.nvim',
-        requires = { 'nvim-tree/nvim-web-devicons', opt = true },
-        config = config("plugin.lualine")
+        "nvim-lualine/lualine.nvim",
+        requires = { "nvim-tree/nvim-web-devicons", opt = true },
+        config = config("plugin.lualine"),
     },
     {
         "SmiteshP/nvim-navic",
         dependencies = { "neovim/nvim-lspconfig" },
-        config = config("plugin.navic")
+        config = config("plugin.navic"),
     },
     {
-        "L3MON4D3/LuaSnip"
+        "L3MON4D3/LuaSnip",
     },
     {
         "RRethy/vim-illuminate",
-        config = config("plugin.illuminate")
+        config = config("plugin.illuminate"),
     },
     {
-        "nvim-lua/popup.nvim"
+        "nvim-lua/popup.nvim",
     },
     {
         "nvimtools/none-ls.nvim",
-        config = config("plugin.none-ls")
+        config = config("plugin.none-ls"),
     },
     {
-        "junegunn/goyo.vim"
-        --require('lualine').hide()
-        --require('lualine').hide({unhide=true})
-    }
+        "junegunn/goyo.vim",
+    },
 })
--- lua/remap.lua
+++ lua/remap.lua
@@ -12,18 +12,24 @@ vim.keymap.set("n", "<C-l>", "<C-w>l")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 
-vim.api.nvim_set_keymap('v', '<Tab>', '>gv', {})
-vim.api.nvim_set_keymap('v', '<S-Tab>', '<gv', {})
+vim.api.nvim_set_keymap("v", "<Tab>", ">gv", {})
+vim.api.nvim_set_keymap("v", "<S-Tab>", "<gv", {})
 
-vim.api.nvim_set_keymap('n', '<A-j>', [[:m .+1<CR>==]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<A-k>', [[:m .-2<CR>==]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', '<A-j>', [[<Esc>:m .+1<CR>==gi]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', '<A-k>', [[<Esc>:m .-2<CR>==gi]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<A-j>', [[:m '>+1<CR>gv=gv]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<A-k>', [[:m '<-2<CR>gv=gv]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<A-j>", [[:m .+1<CR>==]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<A-k>", [[:m .-2<CR>==]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<A-j>", [[<Esc>:m .+1<CR>==gi]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<A-k>", [[<Esc>:m .-2<CR>==gi]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<A-j>", [[:m '>+1<CR>gv=gv]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<A-k>", [[:m '<-2<CR>gv=gv]], { noremap = true, silent = true })
 
 vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
 
--- For local replace
-vim.api.nvim_set_keymap('n', '<leader>r', "gd[{V%::s/<C-R>///gc<left><left><left>", { noremap = true, silent = true })
-
+vim.api.nvim_set_keymap('n', '<F5>', ':Goyo<CR>', { noremap = true, silent = true })
+function Goyo_enter()
+    require("lualine").hide()
+end
+function Goyo_leave()
+    require("lualine").hide({ unhide = true })
+end
+vim.cmd("autocmd! User GoyoEnter nested lua Goyo_enter()")
+vim.cmd("autocmd! User GoyoLeave nested lua Goyo_leave()")
